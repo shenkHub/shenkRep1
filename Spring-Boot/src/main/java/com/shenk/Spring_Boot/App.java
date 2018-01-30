57c227bbb831a076229d4a2cb3a56780f79a8ae8
shenkHub <2388689831@qq.com>
2018-01-30 11:36:36 +0800

    Initial commit

---
+++ Spring-Boot/src/main/java/com/shenk/Spring_Boot/App.java
@@ -0,0 +1,13 @@
+package com.shenk.Spring_Boot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
